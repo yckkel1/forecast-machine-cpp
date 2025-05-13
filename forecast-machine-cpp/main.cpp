028859c457db531bba8f6e0571c1a0ee58597705
yckkel1 <sammwang21@gmail.com>
2025-05-12 22:09:52 -0400

    Initial Commit

---
+++ forecast-machine-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  forecast-machine-cpp
+//
+//  Created by yuxin chen on 5/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
